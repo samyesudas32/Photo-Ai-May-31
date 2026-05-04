1f2a9bce597de24d666cbed5da1b1f208bc4fdf1
Cam Click <camclick04@gmail.com>
2026-05-04 06:45:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7307458626-8e77a",
+  "appId": "1:1008903205774:web:e2f2757b9d76cf0ddd6adf",
+  "apiKey": "AIzaSyAhzE_FVjRkMsVMW2kC6ZMtwRSWfW9U-58",
+  "authDomain": "studio-7307458626-8e77a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1008903205774"
+};
